8b929728ad70add72fa55b7dd4cb652b4286c95a
Vitor Hugo <vitorhugo0031@outlook.com>
2025-05-12 21:07:35 -0300

    test

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -3,14 +3,16 @@ import Button from '../../components/button';
 
 export default function Login() {
 
-
+    const testeButton = () => {
+        alert("Botão clicado");
+    }
 
     return(
         <div className="w-full h-auto">
 
-            <section className="w-full h-[250px]">
-                <figure className='w-full h-full flex justify-center items-center'>
-                    <img src={Logo} alt="Logo" className="w-1/2 h-auto mx-auto mt-10" />
+            <section className="w-full h-auto ">
+                <figure className='w-full  flex justify-center items-center'>
+                    <img src={Logo} alt="Logo" className="w-1/2 max-w-[500px] max-h-[500px] mx-auto " />
                 </figure>
             </section>
 
@@ -24,16 +26,16 @@ export default function Login() {
 
                     <div className='w-full h-auto flex flex-col pl-3 pr-3 gap-1'>
                         <label htmlFor="" className='text-colorPrin'>Email</label>
-                        <input type="text" name="" id="" className='h-[45px] border border-colorInput rounded-lg text-colorInput pl-3' />
+                        <input type="text" name="" id="" className='h-[45px] border border-colorInput rounded-lg text-colorText pl-3' />
                     </div>
 
                     <div className='w-full h-auto flex flex-col pl-3 pr-3 gap-1 mt-8'>
                         <label htmlFor="" className='text-colorPrin'>Senha</label>
-                        <input type="password" name="" id="" className='h-[45px] border border-colorInput rounded-lg text-colorInput pl-3' />
+                        <input type="password" name="" id="" className='h-[45px] border border-colorInput rounded-lg text-colorText pl-3' />
                     </div>
 
                     <div className='w-full h-auto flex justify-center mt-8'>
-                        <Button label="Entrar" />
+                        <Button label="Entrar" onClick={testeButton()}/>
                     </div>
 
                 </form>
