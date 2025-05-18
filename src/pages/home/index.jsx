5c9fa0b406f67f7964a2f7803e49f6bb76a7bf13
Vitor Hugo <vitorhugo0031@outlook.com>
2025-05-18 01:46:03 -0300

    test logof

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,21 @@
-
+import Button from "../../components/button";
+import { signOut } from "firebase/auth";
+import { auth } from "../../database"
 
 export default function Home() {
+
+   async function logof() {
+
+    await signOut(auth);
+    alert("Usuário deslogado com sucesso!");
+        
+        window.location.href = "/login";
+    }
     return (
         <div className="w-full h-auto">
             <h1>Voce logou e esta na home </h1>
+
+            <Button label="logof" onClick={logof}   />
         </div>
     )
 }
