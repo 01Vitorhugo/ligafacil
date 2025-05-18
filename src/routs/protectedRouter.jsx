1f2613b7b8f1a46dca7838ea6736428aa692b7c0
Vitor Hugo <vitorhugo0031@outlook.com>
2025-05-18 01:37:53 -0300

    adjusting

--- src/routs/protectedRouter.jsx
+++ src/routs/protectedRouter.jsx
@@ -4,14 +4,20 @@ import { useEffect, useState } from "react";
 
 export default function ProtectedRoute({ children }) {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged((currentUser) => {
             setUser(currentUser);
+            setLoading(false);
         });
 
         return () => unsubscribe();
     }, []);
+
+    if (loading) {
+        return <p>Carregando...</p>; // Evita redirecionamento antes de verificar estado
+    }
 
     return user ? children : <Navigate to="/login" />;
 }
