8cb77f2d011a83062c3689d6136c219d91e66676
Vitor Hugo <vitorhugo0031@outlook.com>
2025-05-19 01:15:29 -0300

    remove function

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,26 +1,12 @@
-import Button from "../../components/button";
-import { signOut } from "firebase/auth";
-import { auth } from "../../database"
-
-import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
 
-    const navigate = useNavigate();
+  
 
-    async function logof() {
-
-        await signOut(auth);
-        // alert("Usuário deslogado com sucesso!");
-        toast.success("Usuário deslogado com sucesso!");
-
-        navigate('/login');
-    }
+   
     return (
         <div className="w-full h-auto">
             <h1>Voce logou e esta na home </h1>
-
-            <Button label="logof" onClick={logof} />
         </div>
     )
 }
