187c25480ed52b64b28e871178c7029e0915ad9c
Vitor Hugo <vitorhugo0031@outlook.com>
2025-05-19 12:14:35 -0300

    crating page profile

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,11 +1,25 @@
-
+import img from '../../assets/logo_branca.png'
 
 
 export default function Profile() {
 
     return (
-        <>
-        <h1>Profile</h1>
-        </>
+        <div className="w-full h-auto">
+            <section className=" h-20 flex items-center pl-3 border-b-1 border-colorPrinOpacity" >
+                <h1 className="text-[24px]">Meu perfil</h1>
+            </section>
+
+            <figure className="w-full h-auto flex justify-center items-center mt-10">
+                <img src={img} alt="escuto do time" className='w-[173px] h-[173px]'/>
+            </figure>
+
+            <section className='w-full h-auto justify-center flex mt-10 '>
+                <div className='w-[319px] h-[85px] rounded-bl-[50px] rounded-tr-[50px] border-1 border-colorText bg-colorInput flex justify-center items-center'>
+
+                </div>
+            </section>
+
+        </div>
+        
     )
 }
