8678ba0c67922b26cf892aee8957f1ffd1e4b982
Vitor Hugo <vitorhugo0031@outlook.com>
2025-05-21 16:59:13 -0300

    publicgame component

---
+++ src/pages/publicGame/index.jsx
@@ -0,0 +1,12 @@
+
+
+export default function PublicGame() {
+    return (
+        <div className="w-full h-auto">
+            <section className=" h-20 flex items-center pl-3 border-b-1 border-colorPrinOpacity">
+                <h1 className="text-[24px]">Publicar jogo</h1>
+            </section>
+
+        </div>
+    )
+}
