8bff80a4f4c2f9f235b43b2658c459c6a11ffaf0
Vitor Hugo <vitorhugo0031@outlook.com>
2025-05-21 16:46:07 -0300

    test debug

--- src/routs/routs.jsx
+++ src/routs/routs.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter  as Router, Route, Routes } from "react-router-dom";
 import Login from "../pages/login";
 import Register from "../pages/register";
 import Home from "../pages/home";
