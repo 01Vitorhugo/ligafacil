9ac088f03d88eb07b5509c7297e6346b12219896
Vitor Hugo <vitorhugo0031@outlook.com>
2025-05-23 20:14:39 -0300

    test games home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,22 +1,34 @@
-import { useState, useEffect, useContext } from "react";
+import { useContext } from "react";
 import banner from "../../assets/banner_home.png";
 import CardGame from "../../components/cardgame";
 import { Contextapi } from "../../contexApi";
 
 export default function Home() {
-    
-    const {nameTime, streetLocation, dateGame, hoursGame, statusGame, locationGame, numberLocation } = useContext(Contextapi);
+
+  const { gamesDatas } = useContext(Contextapi);
 
 
-    return (
-        <div className="w-full h-auto">
-              <figure className="h-[252px]">
-                <img src={banner} alt="banner home" />
-                
-              </figure>
+  return (
+    <div className="w-full h-auto">
+      <figure className="h-[252px]">
+        <img src={banner} alt="banner home" />
+      </figure>
 
-              <CardGame endereco={locationGame} data={dateGame} horario={hoursGame} nomeTime={nameTime}  rua={streetLocation} numero={numberLocation}/>
 
-        </div>
-    );
+      {gamesDatas.map((game, index) => (
+        <CardGame
+          key={index}
+          endereco={game.bairro}
+          data={game.data}
+          horario={game.horario}
+          nomeTime={game.nomeTime}
+          rua={game.rua}
+          numero={game.numeroEndereco}
+          statusGame={game.status}
+        />
+      ))}
+
+
+    </div>
+  );
 }
