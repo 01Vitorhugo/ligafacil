218b4d12a4b1f0db01e045ca0adde0fce70c2f2c
Vitor Hugo <vitorhugo0031@outlook.com>
2025-05-23 19:20:59 -0300

    add button

--- src/components/cardgame.jsx
+++ src/components/cardgame.jsx
@@ -1,12 +1,13 @@
 import photo from '../assets/default.png'
 import logoIcon from '../assets/logo_icon.png';
+import Button from './button';
 
-export default function CardGame({ horario, data, endereco }) {
+export default function CardGame({ horario, data, endereco, nomeTime, rua, numero, statusGame }) {
 
     return (
         <div className="w-full h-auto flex flex-col items-center justify-center ">
 
-            <div className="bg-cover bg-center w-[90%] h-[381px] rounded-lg shadow-[5px_5px_10px_rgba(255,255,255,0.5)] relative  overflow-hidden">
+            <div className="bg-cover bg-center w-[90%] h-[381px] rounded-lg shadow-[5px_5px_10px_rgba(255,255,255,0.5)] relative  overflow-hidden flex flex-col ">
                 <div
                     className="absolute inset-0 bg-cover bg-center rotate-[-30deg] size-[450px] opacity-20"
                     style={{ backgroundImage: `url(${logoIcon})` }}
@@ -18,13 +19,13 @@ export default function CardGame({ horario, data, endereco }) {
                     </figure>
 
                     <div className='w-[70%] h-full flex items-center justify-center '>
-                        <h2 className='text-colorText'>Loucos do morro</h2>
+                        <h2 className='text-colorText'>{nomeTime}</h2>
 
                     </div>
                 </section>
 
                 <section className="border-b-1 border-colorPrinOpacity h-14 flex items-center p-3">
-                    <p className='text-colorText'>Endereço: {endereco}</p>
+                    <p className='text-colorText'>Endereço: {rua}, {numero} - {endereco}</p>
 
                 </section>
 
@@ -37,7 +38,14 @@ export default function CardGame({ horario, data, endereco }) {
                     <p className='text-colorText'>Horário: {horario}</p>
 
                 </section>
+
+                <div className='h-auto flex items-center justify-center p-6'>
+                    <Button label='Disponível' />
+                </div>
+                
             </div>
+
+
         </div>
     );
 }
