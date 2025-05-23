ae8a4b6eb41eacd4b3e74fa44164f0d8f2a63331
Vitor Hugo <vitorhugo0031@outlook.com>
2025-05-23 17:20:31 -0300

    adjusting the home page to project performance

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,65 +1,20 @@
-import { useState, useEffect } from "react";
-import { auth } from "../../database";
-import { db } from "../../database";
-import { collection, getDocs } from "firebase/firestore";
-import { onAuthStateChanged } from "firebase/auth";
+import { useState, useEffect, useContext } from "react";
 import banner from "../../assets/banner_home.png";
 import CardGame from "../../components/cardgame";
+import { Contextapi } from "../../contexApi";
 
 export default function Home() {
-    const [games, setGames] = useState([]);
-    const [userId, setUserId] = useState(null);
+    
+    const { estado, setEstado } = useContext(Contextapi);
 
-    useEffect(() => {
-        // Escutar mudanças na autenticação
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            if (user) {
-                setUserId(user.uid);
-                fetchGames(user.uid); // Carregar os jogos quando o usuário estiver disponível
-            }
-        });
-
-        return () => unsubscribe(); // Cleanup do listener ao desmontar
-    }, []);
-
-    // Função para buscar os jogos do usuário autenticado
-    const fetchGames = async (userId) => {
-        if (!userId) return;
-
-        try {
-            const gamesRef = collection(db, "users", userId, "games");
-            const snapshot = await getDocs(gamesRef);
-            //console.log(snapshot)
-            const gamesData = snapshot.docs.map(doc => ({
-                id: doc.id,
-                ...doc.data()
-            }));
-
-            setGames(gamesData);
-        } catch (error) {
-            console.error("Erro ao buscar jogos:", error);
-        }
-    };
-
+     
     return (
         <div className="w-full h-auto">
               <figure className="h-[252px]">
                 <img src={banner} alt="banner home" />
+                
               </figure>
 
-        
-
-            {games.length === 0 ? (
-                <p className="text-gray-500">Nenhum jogo publicado ainda.</p>
-            ) : (
-                <ul>
-                    {games.map(game => (
-                        <li key={game.id}>
-                            <CardGame data={game.data} horario={game.horario} />
-                        </li>
-                    ))}
-                </ul>
-            )}
         </div>
     );
 }
