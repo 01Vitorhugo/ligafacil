e09e13a2c4f67d3575b5a6ab871f602ffe59c1e6
Vitor Hugo <vitorhugo0031@outlook.com>
2025-05-23 22:03:32 -0300

    deleting card component

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,33 +1,18 @@
-import { useContext } from "react";
+
 import banner from "../../assets/banner_home.png";
-import CardGame from "../../components/cardgame";
-import { Contextapi } from "../../contexApi";
+
+
 
 export default function Home() {
 
-  const { gamesDatas } = useContext(Contextapi);
+
 
 
   return (
     <div className="w-full h-auto">
       <figure className="h-[252px]">
-        <img src={banner} alt="banner home" />
+        <img src={banner} alt="banner home" className="h-full" />
       </figure>
-
-
-      {gamesDatas.map((game, index) => (
-        <CardGame
-          key={index}
-          endereco={game.bairro}
-          data={game.data}
-          horario={game.horario}
-          nomeTime={game.nomeTime}
-          rua={game.rua}
-          numero={game.numeroEndereco}
-          statusGame={game.status}
-        />
-      ))}
-
 
     </div>
   );
