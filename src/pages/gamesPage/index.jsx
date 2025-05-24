6c100e31695e02da0649db90a1d1e4bf02caf148
Vitor Hugo <vitorhugo0031@outlook.com>
2025-05-23 22:00:00 -0300

    creating gamePage

---
+++ src/pages/gamesPage/index.jsx
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+import { Contextapi } from "../../contexApi";
+import CardGame from "../../components/cardgame";
+
+export default function GamePage() {
+    const { gamesDatas } = useContext(Contextapi);
+
+    return (
+        <section className="w-full h-auto">
+            <section className="w-full h-20 flex items-center p-3 border-b-1 border-colorPrinOpacity ">
+                <h1 className='text-[24px]'>Jogos disponíveis</h1>
+            </section>
+
+            {gamesDatas.map((game, index) => (
+                <CardGame
+                    key={index}
+                    endereco={game.bairro}
+                    data={game.data}
+                    horario={game.horario}
+                    nomeTime={game.nomeTime}
+                    rua={game.rua}
+                    numero={game.numeroEndereco}
+                    statusGame={game.status}
+                />
+            ))}
+        </section>
+    )
+}
