084617eebd91adf266de8663067bd68433a287e9
Vitor Hugo <vitorhugo0031@outlook.com>
2025-05-27 17:13:06 -0300

    adjusting cardgame

--- src/components/cardgame.jsx
+++ src/components/cardgame.jsx
@@ -2,12 +2,12 @@ import photo from '../assets/default.png'
 import logoIcon from '../assets/logo_icon.png';
 import Button from './button';
 
-export default function CardGame({ horario, data, endereco, nomeTime, rua, numero, cep, teste}) {
+export default function CardGame({ horario, data, endereco, nomeTime, rua, numero, cep }) {
 
     return (
         <div className="w-full h-auto flex flex-col items-center justify-center mt-15 ">
 
-            <div className="bg-cover bg-center w-[90%] h-[381px] rounded-lg shadow-[1px_1px_5px_rgba(255,255,255,0.5)] relative  overflow-hidden flex flex-col ">
+            <div className="bg-cover bg-center w-[90%] h-auto rounded-lg shadow-[1px_1px_5px_rgba(255,255,255,0.5)] relative  overflow-hidden flex flex-col ">
                 <div
                     className="absolute inset-0 bg-cover bg-center rotate-[-30deg] size-[450px] opacity-20"
                     style={{ backgroundImage: `url(${logoIcon})` }}
@@ -23,27 +23,23 @@ export default function CardGame({ horario, data, endereco, nomeTime, rua, numer
                     </div>
                 </section>
 
-                <section className="border-b-1 border-colorPrinOpacity h-14 flex items-center p-3">
+                <section className="border-b-1 border-colorPrinOpacity h-auto flex items-center p-3">
                     <p className='text-colorText'> {rua}, {numero} - {endereco}, {cep}</p>
 
                 </section>
 
-                <section className="border-b-1 border-colorPrinOpacity h-14 flex items-center p-3">
+                <section className="border-b-1 border-colorPrinOpacity h-auto flex items-center p-3">
                     <p className='text-colorText'>Data: {data}</p>
 
                 </section>
 
-                <section className="border-b-1 border-colorPrinOpacity h-14 flex items-center p-3">
+                <section className="border-b-1 border-colorPrinOpacity h-auto flex items-center p-3">
                     <p className='text-colorText'>Horário: {horario}</p>
 
                 </section>
 
-                <div className='h-auto flex items-center justify-center p-6'>
-                    <Button label={teste} />
-                </div>
-                
             </div>
-
+        
 
         </div>
     );
